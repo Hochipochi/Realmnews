ff69fa9d4ad29316e3940c8a3f35cf79dd12890a
Michael Smitte <smittemichael@yahoo.com>
2023-03-01 00:44:01 -0500

    initiated application, added a background, created a header

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+      <div className='container header_container'>
+          <h5>Realm News</h5>
+          <h5></h5>
+    </div>
+  )
+}
+
+export default Header
